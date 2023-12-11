8271990286d481316d58a301882e8b4497b9dd1d
sophxmia <maliarenko.sofiia@gmail.com>
2023-12-11 18:03:47 +0200

    Add general look to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import "./App.css";
+import logo from "./logo.svg";
 import TodoWrapper from "./components/TodoWrapper";
 
 function App() {
   return (
     <div className="App">
-      <TodoWrapper />
+      <div className="container-fluid">
+        <div className="App-header">
+          <a
+            href="https://brilliant-meerkat-d49135.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={logo} className="logo" alt="Header Logo" />
+          </a>
+        </div>
+        <TodoWrapper />
+        <footer className="mt-5">
+          Open-sourse project,{" "}
+          <a
+            href="https://github.com/sophxmia/react-dictionary-app"
+            target="_blank"
+            rel="noreferrer"
+            title="GitHub link"
+          >
+            hosted on GitHub
+          </a>
+          , written by{" "}
+          <a
+            href="https://brilliant-meerkat-d49135.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+            title="Netlify link to the portfolio"
+          >
+            Sofiia Maliarenko
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function TodoWrapper() {
-  return <div className="TodoWrapper"></div>;
+  return (
+    <div className="TodoWrapper">
+      <h1>You handy Todo app</h1>
+      <form>
+        <input type="text" placeholder="Enter a task"></input>
+        <button>Add Todo</button>
+      </form>
+      <ul>
+        <li>task 1</li>
+        <li>task 2</li>
+      </ul>
+    </div>
+  );
 }
