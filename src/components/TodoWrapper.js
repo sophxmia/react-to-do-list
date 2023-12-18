57e71744a7c420c82f7de670dd395fd140b86a5f
sophxmia <maliarenko.sofiia@gmail.com>
2023-12-18 23:19:24 +0200

    Added local storage saving option

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TodoForm from "./TodoForm";
 import Todo from "./Todo";
 import { v4 as uuidv4 } from "uuid";
 uuidv4();
 
 export default function TodoWrapper() {
-  const [todos, setTodo] = useState([]);
+  const [todos, setTodo] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    if (savedTodos) {
+      return JSON.parse(savedTodos);
+    } else {
+      return [];
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   function addTodo(todo) {
     setTodo([
